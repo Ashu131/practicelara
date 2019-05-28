df75aa1aa7caf0fde9376f6ff5b7b117c1fdc64d
Ashutosh <ashutosh.sharma366@gmail.com>
2019-05-28 08:43:03 +0530

    Initial Commit

---
+++ database/factories/LessonFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator as Faker;
+use App\Model\Lesson;
+
+$factory->define(Lesson::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(4),
+        'body'  => $faker->text()
+    ];
+});
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Model\Lesson;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Lesson::truncate();
+        
+        Model::unguard();
+            $this->call(LessonsTableSeeder::class);
+        Model::reguard();
+    }
+}
