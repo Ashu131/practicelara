3ae62cbd0f7857a5c5f37374b2b5f439ca695ed5
Ashutosh <ashutosh.sharma366@gmail.com>
2019-06-02 14:41:51 +0530

    can attach meta data and http status code

---
+++ app/Http/Controllers/Api/LessonsController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Model\Lesson;
+
+class LessonsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // All() is bad
+        // No way to attach meta data
+        // Linking DB structure to the API Output
+        // NO way to signal headers response code
+        $lessons= Lesson::all();
+        return response()->json([
+            'data'  =>$lessons
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $lesson= Lesson::find($id);
+        if (!$lesson) {
+            return response()->json([
+                'error' =>'Lesson not found.'
+            ], 404);
+        }
+
+        return response()->json([
+            'data'  =>$lesson
+        ], 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
